c0dc9dda95a164754540f73c978976c98d11c06a
Dave Conway-Jones <dceejay@gmail.com>
2023-08-14 09:00:08 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# node-red-contrib-fastxml
-Node-RED node that convert from XML to JSON and vice versa.
+node-red-contrib-fastxml
+========================
+
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to parse XML strings into JSON and vice-versa. 
+
+
+Install
+-------
+
+Run the following command in your Node-RED user directory - typically `~/.node-red`
+
+    npm i node-red-contrib-fastxml
+
+Usage
+-----
+
+A function that converts the `msg.payload` to and from XML format.
+
+Uses the fast-xml-parser library.
---
+++ fastxml.js
@@ -0,0 +1,44 @@
+
+module.exports = function(RED) {
+    "use strict";
+    const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
+
+    function fastXMLnode(n) {
+        RED.nodes.createNode(this,n);
+        this.attrkey = n.attr;
+        this.charkey = n.chr;
+        this.property = n.property||"payload";
+        var node = this;
+        this.on("input", function(msg) {
+            var value = RED.util.getMessageProperty(msg,node.property);
+            if (value !== undefined) {
+                var options = {};
+                if (Buffer.isBuffer(value)) { value = value.toString(); }
+                if (typeof value == "string") {
+                    options.ignoreAttributes = false;
+                    options.attributeNamePrefix = "";
+                    if (msg.hasOwnProperty("options") && typeof msg.options === "object") { options = msg.options; }
+                    // options.async = true;
+                    options.attrNodeName = node.attrkey || options.attrkey || '';
+                    options.textNodeName = node.charkey || options.charkey || '_';
+                    const parser = new XMLParser(options);
+                    value = parser.parse(value);
+                    RED.util.setMessageProperty(msg,node.property,value);
+                    node.send(msg);
+                }
+                else if (typeof value === "object") {
+                    // options = {renderOpts:{pretty:false}};
+                    if (msg.hasOwnProperty("options") && typeof msg.options === "object") { options = msg.options; }
+                    options.async = false;
+                    const builder = new XMLBuilder(options);
+                    value = builder.build(value);
+                    RED.util.setMessageProperty(msg,node.property,value);
+                    node.send(msg);
+                }
+                else { node.warn(RED._("xml.errors.xml_js")); }
+            }
+            else { node.send(msg); } // If no property - just pass it on.
+        });
+    }
+    RED.nodes.registerType("fastxml",fastXMLnode);
+}
